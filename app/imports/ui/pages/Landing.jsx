1ae1ed7d7d8852c547a4095b697e5fe459fba1f3
Joseph Paragas <josephparagas5@gmail.com>
2020-06-06 12:11:14 -1000

    added background to landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,18 +5,9 @@ import { Grid, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='landing-background'>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/logo_sq.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to Task Jeeves</h1>
-            <p>Now get to work and make some tasks!</p>
-          </Grid.Column>
-
-        </Grid>
+        </div>
     );
   }
 }
