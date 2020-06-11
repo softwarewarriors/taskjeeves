c92d753815070ddf3632362bdffcb83e160a24b8
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2020-06-10 15:47:50 -1000

    Update sign in page
    Add background image
    Change color of button
    Add message for welcome back/sign in
    Add warning to login if user already has an account

---
+++ Signin.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, Redirect } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
+import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+
+/**
+ * Signin page overrides the form’s submit event and call Meteor’s loginWithPassword().
+ * Authentication errors modify the component’s state to be displayed
+ */
+export default class Signin extends React.Component {
+
+  /** Initialize component state with properties for login and redirection. */
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '', error: '', redirectToReferer: false };
+  }
+
+  /** Update the form controls each time the user interacts with them. */
+  handleChange = (e, { name, value }) => {
+    this.setState({ [name]: value });
+  }
+
+  /** Handle Signin submission using Meteor's account mechanism. */
+  submit = () => {
+    const { email, password } = this.state;
+    Meteor.loginWithPassword(email, password, (err) => {
+      if (err) {
+        this.setState({ error: err.reason });
+      } else {
+        this.setState({ error: '', redirectToReferer: true });
+      }
+    });
+  }
+
+  /** Render the signin form. */
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } };
+    // if correct authentication, redirect to page instead of login screen
+    if (this.state.redirectToReferer) {
+      return
+    <Redirect to={from}/>
+;
+    }
+    // Otherwise return the Login form.
+    return (
+<Container height="100vh" style={{ margin: '0px', width: '100%', height: '110vh', backgroundColor: 'white', backgroundSize: 'cover', backgroundImage: `url(${"/images/signin-background.jpg"})` }}>
+  <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
+    <Grid.Column style={{ paddingTop: '30vh'}}>
+      <Message
+      attached
+      header='Welcome back!'
+      content='Input your email and password to login to your account.'
+    />
+      <Form className='attached fluid segment' onSubmit={this.submit}>
+        <Form.Input
+                  label="Email"
+                  icon="user"
+                  iconPosition="left"
+                  name="email"
+                  type="email"
+                  placeholder="E-mail address"
+                  onChange={this.handleChange}
+                />
+        <Form.Input
+                  label="Password"
+                  icon="lock"
+                  iconPosition="left"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                  onChange={this.handleChange}
+                />
+        <Grid>
+          <Grid.Column textAlign="center">
+            <Form.Button style={{borderRadius: '10px', backgroundColor: '#8fd7ce', color: '#fff'}} content="Submit"/>
+          </Grid.Column>
+        </Grid>
+      </Form>
+      <Message attached='bottom' info> Don't have an account yet? Click&nbsp;
+        <Link to="/signup">
+        here
+        </Link>
+        &nbsp;to register instead. </Message>
+      {this.state.error === '' ? (
+      ''
+      ) : (
+      <Message
+                error
+                header="Login was not successful"
+                content={this.state.error}
+              />
+      )} </Grid.Column>
+  </Grid>
+</Container>
+);
+  }
+}
+
+/** Ensure that the React Router location object is available in case we need to redirect. */
+Signin.propTypes = {
+  location: PropTypes.object,
+}; 
