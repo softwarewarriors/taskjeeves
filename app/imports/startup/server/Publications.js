da7ae38cfa5a568d0ef6886af22c03e66a0ef34f
Craig Opie <craigopie@gmail.com>
2020-06-14 02:17:05 -1000

    added user table for first and last name, email, and address.

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
 import { Task } from '../../api/task/Task';
+import { User } from '../../api/user/User';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Stuff', function publish() {
@@ -17,6 +18,15 @@ Meteor.publish('Tasks', function publish() {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Task.find({ owner: username });
+  }
+  return this.ready();
+});
+
+/** This subscription publishes only the information associated with the logged in user */
+Meteor.publish('User', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return User.find({ email: username });
   }
   return this.ready();
 });
